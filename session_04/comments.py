a65d6e82ce3d85d546a01c6ae4bed135bf343e62
juliagodinez30 <jcgodinez@ucdavis.edu>
2022-02-01 14:59:40 -0800

    session_04

---
+++ session_04/comments.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+print ("Here is an example of an inline comments") # Inline comment
+# Print my name
+print ("Julia")
+'''#Prints numbers 1-20
+for i in range (1,21): print (i)'''
+#Prints the names of all my pets
+pet_names = ["Bonnie," "Sweetie Pie," "Pasha"]
+for pet in pet_names: print (pet) 
+
